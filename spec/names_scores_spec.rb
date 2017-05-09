64b8e500584aa50ae320e9e1b5e3adb36a83b638
Basile Koko <basile_koko@hotmail.com>
2017-05-09 11:25:38 +0100

    adding test: it should sort the names by alphabetical order

--- spec/names_scores_spec.rb
+++ spec/names_scores_spec.rb
@@ -6,4 +6,11 @@ describe NameScore do
       expect(subject.file_to_array('../names_scores/names_test.txt')).to eq ["TOM", "SHEER", "JERI"]
     end
   end
+  
+  describe '#sort_names' do
+    it 'should sort the names by alphabetical order' do
+      names_array = ["TOM", "SHEER", "JERI"]
+      expect(subject.sort_names(names_array)).to eq ["JERI", "SHEER", "TOM"]
+    end
+  end
 end
