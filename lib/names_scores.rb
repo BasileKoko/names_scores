2dea6af80198368a0070173b77133f7ca7dc4a9e
Basile Koko <basile_koko@hotmail.com>
2017-05-09 11:38:57 +0100

    passing test: it should sort the names by alphabetical order

--- lib/names_scores.rb
+++ lib/names_scores.rb
@@ -1,8 +1,12 @@
 class NameScore
 
- def file_to_array(file='names.txt')
-   read_file = File.read(file)
-   remove_quote = read_file.gsub(/\"/, "").strip
-   names_array = remove_quote.split(',')
- end
+  def file_to_array(file='names.txt')
+    read_file = File.read(file)
+    remove_quote = read_file.gsub(/\"/, "").strip
+    names_array = remove_quote.split(',')
+  end
+
+  def sort_names(names_array)
+    names_array.sort
+  end
 end
