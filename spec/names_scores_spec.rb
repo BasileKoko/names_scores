d4abcf727247d93136edb1d1bf021b8184a3e418
Basile Koko <basile_koko@hotmail.com>
2017-05-09 11:44:41 +0100

    adding test: it should return the value of each name

--- spec/names_scores_spec.rb
+++ spec/names_scores_spec.rb
@@ -6,11 +6,18 @@ describe NameScore do
       expect(subject.file_to_array('../names_scores/names_test.txt')).to eq ["TOM", "SHEER", "JERI"]
     end
   end
-  
+
   describe '#sort_names' do
     it 'should sort the names by alphabetical order' do
       names_array = ["TOM", "SHEER", "JERI"]
       expect(subject.sort_names(names_array)).to eq ["JERI", "SHEER", "TOM"]
+    end
+  end
+
+  describe '#name_value' do
+    it 'should return the value of each name' do
+      sorted_names_array = ["JERI", "SHEER", "TOM"]
+      expect(subject.name_value(sorted_names_array)).to eq [42, 55, 48]
     end
   end
 end
